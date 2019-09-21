45a16f5392455e863418c672a6f913bf0b6d3948
Camposm97 <camposm1205@gmail.com>
2019-09-20 20:14:42 -0400

    Modified Demo.java and Created MainPage.java

--- src/app/Demo.java
+++ src/app/Demo.java
@@ -1,7 +1,15 @@
 package app;
 
-public class Demo {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Demo extends Application {
+	@Override
+	public void start(Stage stage) throws Exception {
+		stage.show();
+	}
+	
 	public static void main(String[] args) {
-		
+		launch(args);
 	}
 }
